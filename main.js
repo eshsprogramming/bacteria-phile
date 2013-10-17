22b17b60a4f8c29d28939cf79c43ef6cd65b0789
Benjamin Landers <benjaminrlanders@gmail.com>
2013-10-17 15:35:10 -0700

    added animation

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 var canvas = document.getElementById('mycanvas');
 var ctx = canvas.getContext('2d');
 var image = new Image();
+var x = 0;
 image.onload = function(){
-	console.log(ctx)
-	ctx.drawImage(this,0,0,this.width,this.height);
+	render();
+	
+}
+
+function render(){
+	requestAnimationFrame(render);
+	ctx.drawImage(image,x,0,image.width,image.height);
+	x++;
 }
 image.src = "bacteria.png";
