b66ee6bd60279bb6e4bad4837a49352478acf4a4
dayliss <dayliss413@gmail.com>
2013-10-17 16:07:51 -0700

    moves on mouse click

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 var canvas = document.getElementById('mycanvas');
 var ctx = canvas.getContext('2d');
-var image = new Image();
 var x = 0;
-image.onload = function(){
-	render();
-	
-}
+var bacteria = new Bacteria();
 
 function render(){
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	bacteria.draw();	
 	requestAnimationFrame(render);
-	ctx.drawImage(image,x,0,image.width,image.height);
-	x++;
+			
 }
-image.src = "bacteria.png";
+
+function Bacteria(){
+	this.x = 0;
+	this.y = 0;
+	this.xvel = 1;
+	this.yvel = 1;
+	this.image = new Image();
+	this.image.src = "bacteria.png"
+	this.draw = function() {
+		ctx.drawImage(this.image,this.x,this.y,this.image.width,this.image.height);
+		this.x += this.xvel;
+		this.y += this.yvel;	
+	}
+	this.image.onload = function(){
+		render();
+	}
+}
+
+canvas.onmousedown = function(e){
+	bacteria.xvel = (e.x - bacteria.x)/100;
+	bacteria.yvel = (e.y - bacteria.y)/100;
+}
